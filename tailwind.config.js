80a53d089257e650f19b6af1aa83fe3455e211b2
Ayshasafna8137 <ayishasafna02@gmail.com>
2026-04-03 10:26:50 +0530

    hosta

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        ring: "hsl(var(--ring))",
+      },
+    },
   },
   plugins: [],
 }
